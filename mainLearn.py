f0200981f481793b3298a504d50f0da8d6aa1067
MaximeDESB <maxime.desbois1@gmail.com>
2019-01-11 15:22:34 +0100

    load Q_learning ne fonctionne pas2

--- mainLearn.py
+++ mainLearn.py
@@ -7,17 +7,34 @@ import kernel
 import csv
 from interface import LearningInterface
 
-
 if __name__ == '__main__':
-    game = kernel.Dungeon(8, 8)
+    game = kernel.Dungeon(4, 4)
     inter = LearningInterface(game)
     player, = game.agents
 
-    game.load_map("default_map.txt")
+    game.load_map("map_mur.txt")
 
+    inter.display()
+    q_table = player.Q
+    game.reset()
+    player.load_Qtable(q_table)
+    a = []
+    while len(a) < 10:
+        k = 0
+        q_table = player.Q
+        game.reset()
+        player.load_Qtable(q_table)
+        while not game.over:
+            game.caption = ''
+            action = player.policy()
+            reward = game.move(player, action)
+            k +=1
+        if player.alive:
+            a.append(k)
+    print(np.mean(a))
 
-    for j in range(6):
-        for i in range(2000):
+    for j in range(5):
+        for i in range(5000):
             q_table = player.Q
             game.reset()
             player.load_Qtable(q_table)
@@ -29,12 +46,12 @@ if __name__ == '__main__':
                 new_state = player.state
                 player.process_reward(old_state, new_state, action, reward)
             if i % 100 == 0:
-                print(i*(j+1))
+                print(i * (j + 1))
         q_table = player.Q
         game.reset()
         player.load_Qtable(q_table)
         a = []
-        for x in range(10):
+        while len(a) < 10:
             k = 0
             q_table = player.Q
             game.reset()
@@ -42,12 +59,13 @@ if __name__ == '__main__':
             while not game.over:
                 game.caption = ''
                 action = player.policy()
-                reward = game.move(player, action)
-                k +=1
-            a.append(k)
+                game.move(player, action)
+                k += 1
+            if player.alive:
+                a.append(k)
         a = np.mean(a)
 
-        path = "data/Qtable_",str((i+1)*(j+1)),".csv"
+        path = "data/Qtable_", str((i + 1) * (j + 1)), ".csv"
         path = ''.join(path)
         path2 = "data/result_", str((i + 1) * (j + 1)), ".txt"
         path2 = ''.join(path2)
@@ -55,9 +73,8 @@ if __name__ == '__main__':
             writer = csv.writer(csvFile)
             writer.writerows(player.Q)
         with open(path2, "w") as file:
-            text = ''.join(("nombre iter pour ",str((i + 1) * (j + 1))," : " ,str(a)))
+            text = ''.join(("nombre iter pour ", str((i + 1) * (j + 1)), " : ", str(a)))
             file.write(text)
-
 
 
 
@@ -68,4 +85,3 @@ if __name__ == '__main__':
     # player.save_Qtable("Qtable.csv")
     # inter.play_game()
     # game.map.save_map("map.txt")
-
